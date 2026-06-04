b8248f0f158682d63254acc82bbd4ad20b396be3
ppsandwich <dfgibbs@gmail.com>
2026-06-04 12:11:35 +1000

    Update demo songs with full lyrics

--- src/lib/demoSongs.ts
+++ src/lib/demoSongs.ts
@@ -33,7 +33,62 @@ If the high was worth the pain
 Got a long list of ex-lovers
 They'll tell you I'm insane
 'Cause you know I love the players
-And you love the game`
+And you love the game
+
+[Verse 3]
+Cherry lips, crystal skies
+I could show you incredible things
+Stolen kisses, pretty lies
+You're the King, baby, I'm your Queen
+Find out what you want
+Be that girl for a month
+Wait, the worst is yet to come, oh no
+
+[Verse 4]
+Screaming, crying, perfect storms
+I can make all the tables turn
+Rose garden filled with thorns
+Keep you second guessing like
+"Oh my God, who is she?"
+I get drunk on jealousy
+But you'll come back each time you leave
+'Cause, darling, I'm a nightmare dressed like a daydream
+
+[Chorus]
+So it's gonna be forever
+Or it's gonna go down in flames
+You can tell me when it's over, mm
+If the high was worth the pain
+Got a long list of ex-lovers
+They'll tell you I'm insane
+'Cause you know I love the players
+And you love the game
+
+[Bridge]
+Boys only want love if it's torture
+Don't say I didn't, say I didn't warn ya
+Boys only want love if it's torture
+Don't say I didn't, say I didn't warn ya
+
+[Chorus]
+So it's gonna be forever
+Or it's gonna go down in flames
+You can tell me when it's over, mm
+If the high was worth the pain
+Got a long list of ex-lovers
+They'll tell you I'm insane
+'Cause you know I love the players
+And you love the game
+
+[Outro]
+'Cause we're young and we're reckless
+We'll take this way too far
+It'll leave you breathless, mm
+Or with a nasty scar
+Got a long list of ex-lovers
+They'll tell you I'm insane
+But I've got a blank space, baby
+And I'll write your name`
   },
   {
     title: "Shape of You",
@@ -47,6 +102,16 @@ And trust me I'll give it a chance now
 Take my hand, stop, put Van the Man on the jukebox
 And now I'm singing like
 
+[Pre-Chorus]
+Girl, you know I want your love
+Your love was handmade for somebody like me
+Come on now, follow my lead
+I may be crazy, don't mind me
+Say, boy, let's not talk too much
+Grab on my waist and put that body on me
+Come on now, follow my lead
+Come, come on now, follow my lead
+
 [Chorus]
 I'm in love with the shape of you
 We push and pull like a magnet do
@@ -57,27 +122,128 @@ And now my bedsheets smell like you
 Every day discovering something brand new
 I'm in love with your body
 Oh I, oh I, oh I, oh I
+I'm in love with your body
+Oh I, oh I, oh I, oh I
+I'm in love with your body
+Oh I, oh I, oh I, oh I
+I'm in love with your body
+Every day discovering something brand new
+I'm in love with the shape of you
+
+[Verse 2]
+One week in, we let the story begin
+We're going out on our first date
+You and me are thrifty, so go all you can eat
+Fill up your bag and I fill up a plate
+We talk for hours and hours about the sweet and the sour
+And how your family is doing okay
+Leave and get in a taxi, then kiss in the backseat
+Tell the driver, "Make the radio play," and I'm singing like
+
+[Pre-Chorus]
+Girl, you know I want your love
+Your love was handmade for somebody like me
+Come on now, follow my lead
+I may be crazy, don't mind me
+Say, boy, let's not talk too much
+Grab on my waist and put that body on me
+Come on now, follow my lead
+Come, come on now, follow my lead
+
+[Chorus]
+I'm in love with the shape of you
+We push and pull like a magnet do
+Although my heart is falling too
+I'm in love with your body
+And last night you were in my room
+And now my bedsheets smell like you
+Every day discovering something brand new
+I'm in love with your body
+Oh I, oh I, oh I, oh I
+I'm in love with your body
+Oh I, oh I, oh I, oh I
+I'm in love with your body
+Oh I, oh I, oh I, oh I
+I'm in love with your body
+Every day discovering something brand new
+I'm in love with the shape of you
+
+[Bridge]
+Come on, be my baby, come on
+Come on, be my baby, come on
+Come on, be my baby, come on
+Come on, be my baby, come on
+Come on, be my baby, come on
+Come on, be my baby, come on
+Come on, be my baby, come on
+Come on, be my baby, come on
+
+[Chorus]
+I'm in love with the shape of you
+We push and pull like a magnet do
+Although my heart is falling too
+I'm in love with your body
+And last night you were in my room
+And now my bedsheets smell like you
+Every day discovering something brand new
 I'm in love with your body`
   },
   {
     title: "Blinding Lights",
-    lyrics: `[Verse 1]
+    lyrics: `[Intro]
 Yeah
+
+[Verse 1]
+I've been tryna call
 I've been on my own for long enough
 Maybe you can show me how to love, maybe
 I'm going through withdrawals
 You don't even have to do too much
 You can turn me on with just a touch, baby
 
-[Chorus]
+[Pre-Chorus]
 I look around and Sin City's cold and empty
 No one's around to judge me
 I can't see clearly when you're gone
+
+[Chorus]
 I said, ooh, I'm blinded by the lights
 No, I can't sleep until I feel your touch
 I said, ooh, I'm drowning in the night
 Oh, when I'm like this, you're the one I trust
-(Hey, hey, hey)`
+(Hey, hey, hey)
+
+[Verse 2]
+I'm running out of time
+'Cause I can see the sun light up the sky
+So I hit the road in overdrive, baby, oh
+
+[Pre-Chorus]
+The city's cold and empty
+No one's around to judge me
+I can't see clearly when you're gone
+
+[Chorus]
+I said, ooh, I'm blinded by the lights
+No, I can't sleep until I feel your touch
+I said, ooh, I'm drowning in the night
+Oh, when I'm like this, you're the one I trust
+(Hey, hey, hey)
+
+[Bridge]
+I'm just calling back to let you know (Back to let you know)
+I could never say it on the phone (Say it on the phone)
+Will never let you go this time
+
+[Chorus]
+I said, ooh, I'm blinded by the lights
+No, I can't sleep until I feel your touch
+(Hey, hey, hey)
+(Hey, hey, hey)
+
+[Outro]
+I said, ooh, I'm blinded by the lights
+No, I can't sleep until I feel your touch`
   },
   {
     title: "Rolling in the Deep",
@@ -86,14 +252,16 @@ There's a fire starting in my heart
 Reaching a fever pitch and it's bringing me out the dark
 Finally, I can see you crystal clear
 Go ahead and sell me out and I'll lay your ship bare
-See how I'll leave with every piece of you
+See how I leave with every piece of you
 Don't underestimate the things that I will do
 
-[Chorus]
+[Pre-Chorus]
 The scars of your love remind me of us
 They keep me thinking that we almost had it all
 The scars of your love, they leave me breathless
 I can't help feeling
+
+[Chorus]
 We could have had it all
 (You're gonna wish you never had met me)
 Rolling in the deep
@@ -101,11 +269,64 @@ Rolling in the deep
 You had my heart inside of your hand
 (You're gonna wish you never had met me)
 And you played it to the beat
-(Tears are gonna fall, rolling in the deep)`
+(Tears are gonna fall, rolling in the deep)
+
+[Verse 2]
+Baby, I have no story to be told
+But I've heard one of you and I'm gonna make your head burn
+Think of me in the depths of your despair
+Make a home down there, as mine sure won't be shared
+
+[Pre-Chorus]
+The scars of your love remind me of us
+They keep me thinking that we almost had it all
+The scars of your love, they leave me breathless
+I can't help feeling
+
+[Chorus]
+We could have had it all
+(You're gonna wish you never had met me)
+Rolling in the deep
+(Tears are gonna fall, rolling in the deep)
+You had my heart inside of your hand
+(You're gonna wish you never had met me)
+And you played it to the beat
+(Tears are gonna fall, rolling in the deep)
+
+[Bridge]
+Throw your soul through every open door
+Turn my sorrow into treasured gold
+You'll pay me back in kind and reap just what you sow
+(You're gonna wish you never had met me)
+(Tears are gonna fall, rolling in the deep)
+(You're gonna wish you never had met me)
+(Tears are gonna fall, rolling in the deep)
+
+[Chorus]
+We could have had it all
+(You're gonna wish you never had met me)
+Rolling in the deep
+(Tears are gonna fall, rolling in the deep)
+You had my heart inside of your hand
+(You're gonna wish you never had met me)
+And you played it to the beat
+(Tears are gonna fall, rolling in the deep)
+
+[Outro]
+We could have had it all
+Rolling in the deep
+You had my heart inside of your hand
+But you played it, you played it, you played it, you played it to the beat`
   },
   {
     title: "Uptown Funk",
-    lyrics: `[Verse 1]
+    lyrics: `[Intro]
+Doh, doh-doh-doh, doh-doh-doh, doh-doh
+Doh, doh-doh-doh, doh-doh-doh, doh-doh
+Doh, doh-doh-doh, doh-doh-doh, doh-doh
+Doh-doh-doh, doh-doh
+
+[Verse 1]
 This hit, that ice cold
 Michelle Pfeiffer, that white gold
 This one for them hood girls
@@ -113,6 +334,16 @@ Them good girls, straight masterpieces
 Stylin', wilin', livin' it up in the city
 Got Chucks on with Saint Laurent
 Gotta kiss myself, I'm so pretty
+
+[Pre-Chorus]
+I'm too hot (hot damn)
+Call the police and the fireman
+I'm too hot (hot damn)
+Make a dragon wanna retire, man
+I'm too hot (hot damn)
+Say my name, you know who I am
+I'm too hot (hot damn)
+And my band 'bout that money, break it down
 
 [Chorus]
 Girls hit your hallelujah (whoo)
@@ -122,14 +353,79 @@ Girls hit your hallelujah (whoo)
 'Cause uptown funk gon' give it to you
 'Cause uptown funk gon' give it to you
 Saturday night and we in the spot
-Don't believe me, just watch (come on)
+Don't believe me, just watch
+Don't believe me, just watch
+Don't believe me, just watch
+Don't believe me, just watch
+Don't believe me, just watch
+Don't believe me, just watch
+
+[Verse 2]
+Stop! Wait a minute
+Fill my cup, put some liquor in it
+Take a sip, sign the check
+Julio, get the stretch
+Ride to Harlem, Hollywood, Jackson, Mississippi
+If we show up, we gon' show out
+Smoother than a fresh jar of Skippy
+
+[Pre-Chorus]
+I'm too hot (hot damn)
+Call the police and the fireman
+I'm too hot (hot damn)
+Make a dragon wanna retire, man
+I'm too hot (hot damn)
+Say my name, you know who I am
+I'm too hot (hot damn)
+And my band 'bout that money, break it down
+
+[Chorus]
+Girls hit your hallelujah (whoo)
+Girls hit your hallelujah (whoo)
+Girls hit your hallelujah (whoo)
+'Cause uptown funk gon' give it to you
+'Cause uptown funk gon' give it to you
+'Cause uptown funk gon' give it to you
+Saturday night and we in the spot
+Don't believe me, just watch
+Don't believe me, just watch
+Don't believe me, just watch
+Don't believe me, just watch
+Don't believe me, just watch
+Don't believe me, just watch
+
+[Bridge]
+Before we leave
+Imma tell y'all a lil' somethin'
+Uptown funk you up, uptown funk you up
+Uptown funk you up, uptown funk you up
+Uptown funk you up, uptown funk you up
+Uptown funk you up, uptown funk you up
+Dance, jump on it
+If you sexy then flaunt it
+If you freaky then own it
+Don't brag about it, come show me
+
+[Outro]
+Uptown funk you up, uptown funk you up
+Uptown funk you up, uptown funk you up
+Uptown funk you up, uptown funk you up
+Uptown funk you up, uptown funk you up
+Saturday night and we in the spot
+Don't believe me, just watch
+Don't believe me, just watch
+Don't believe me, just watch
+Don't believe me, just watch
+Don't believe me, just watch
 Don't believe me, just watch`
   },
   {
     title: "Dynamite",
-    lyrics: `[Verse 1]
+    lyrics: `[Intro]
 'Cause I, I, I'm in the stars tonight
 So watch me bring the fire and set the night alight
+
+[Verse 1]
 Shoes on, get up in the morn'
 Cup of milk, let's rock and roll
 King Kong, kick the drum, rolling on like a Rolling Stone
@@ -138,11 +434,49 @@ Jump up to the top, LeBron
 Ding-dong, call me on my phone
 Ice tea and a game of ping-pong
 
+[Pre-Chorus]
+This is getting heavy, can you hear the bass boom? I'm ready
+Life is sweet as honey, yeah, this beat cha-ching like money
+Disco overload, I'm into that, I'm good to go
+I'm diamond, you know I glow up
+Let's go
+
 [Chorus]
 'Cause I, I, I'm in the stars tonight
 So watch me bring the fire and set the night alight
 Shining through the city with a little funk and soul
-So I'ma light it up like dynamite, woah`
+So I'ma light it up like dynamite, woah-oh-oh
+
+[Verse 2]
+Bring a friend, join the crowd, whoever wanna come along
+Word up, talk the talk, just move like we off the wall
+Day or night, the sky's alight, so we dance to the break of dawn
+Ladies and gentlemen, I got the medicine so you should keep ya eyes on the ball
+
+[Pre-Chorus]
+This is getting heavy, can you hear the bass boom? I'm ready
+Life is sweet as honey, yeah, this beat cha-ching like money
+Disco overload, I'm into that, I'm good to go
+I'm diamond, you know I glow up
+Let's go
+
+[Chorus]
+'Cause I, I, I'm in the stars tonight
+So watch me bring the fire and set the night alight
+Shining through the city with a little funk and soul
+So I'ma light it up like dynamite, woah-oh-oh
+
+[Bridge]
+Dy-na-na-na, na-na-na-na-na, dy-na-na-na, life is dynamite
+Dy-na-na-na, na-na-na-na-na, dy-na-na-na, life is dynamite
+Shining through the city with a little funk and soul
+So I'ma light it up like dynamite, woah-oh-oh
+
+[Chorus]
+'Cause I, I, I'm in the stars tonight
+So watch me bring the fire and set the night alight
+Shining through the city with a little funk and soul
+So I'ma light it up like dynamite, woah-oh-oh`
   },
   {
     title: "Flowers",
@@ -151,7 +485,10 @@ We were good, we were gold
 Kinda dream that can't be sold
 We were right 'til we weren't
 Built a home and watched it burn
-Mm, I didn't wanna leave you, I didn't wanna lie
+
+[Pre-Chorus]
+Mm, I didn't wanna leave you
+I didn't wanna lie
 Started to cry, but then remembered I
 
 [Chorus]
@@ -161,7 +498,52 @@ Talk to myself for hours
 Say things you don't understand
 I can take myself dancing
 And I can hold my own hand
-Yeah, I can love me better than you can`
+Yeah, I can love me better than you can
+
+[Post-Chorus]
+Can love me better, I can love me better, baby
+Can love me better, I can love me better, baby
+
+[Verse 2]
+Paint my nails, cherry-red
+Match the roses that you left
+No remorse, no regret
+I forgive every word you said
+
+[Pre-Chorus]
+Ooh, I didn't wanna leave you, baby
+I didn't wanna fight
+Started to cry, but then remembered I
+
+[Chorus]
+I can buy myself flowers
+Write my name in the sand
+Talk to myself for hours, yeah
+Say things you don't understand
+I can take myself dancing, yeah
+I can hold my own hand
+Yeah, I can love me better than you can
+
+[Bridge]
+Can love me better, I can love me better, baby
+Can love me better, I can love me better, baby
+Can love me better, I can love me better, baby
+Can love me better, I
+
+[Chorus]
+I can buy myself flowers
+Write my name in the sand
+Talk to myself for hours
+Say things you don't understand
+I can take myself dancing, yeah
+I can hold my own hand
+Yeah, I can love me better than you can
+
+[Outro]
+Can love me better, I can love me better, baby
+Can love me better, I can love me better, baby
+Can love me better, I can love me better, baby
+Can love me better, I`
   },
   {
     title: "Bad Guy",
@@ -182,32 +564,103 @@ Just can't get enough guy
 Chest always so puffed guy
 I'm that bad type
 Make your mama sad type
-Make your girlfriend mad tight
+Make your girlfriend mad type
 Might seduce your dad type
 I'm the bad guy
-Duh`
+Duh
+
+[Verse 2]
+I like it when you take control
+Even if you know that you don't
+Own me, I'll let you play the role
+I'll be your animal
+My mommy likes to sing along with me
+But she won't sing this song
+If she reads all the lyrics
+She'll pity the men I know
+
+[Chorus]
+So you're a tough guy
+Like it really rough guy
+Just can't get enough guy
+Chest always so puffed guy
+I'm that bad type
+Make your mama sad type
+Make your girlfriend mad type
+Might seduce your dad type
+I'm the bad guy
+Duh
+
+[Bridge]
+I like when you get mad
+I guess I'm pretty glad that you're alone
+You said she's scared of me?
+I mean, I don't see what she sees
+But maybe it's 'cause I'm wearing your cologne
+
+[Outro]
+I'm the bad guy, duh
+I'm only good at being bad, bad
+I'm a bad guy
+I'm a bad guy`
   },
   {
     title: "Stay",
-    lyrics: `[Verse 1]
+    lyrics: `[Chorus]
 I do the same thing I told you that I never would
 I told you I'd change, even when I knew I never could
 I know that I can't find nobody else as good as you
-I need you to stay, need you to stay, yeah
+I need you to stay, need you to stay, hey
 
-[Chorus]
+[Verse 1]
 I get drunk, wake up, I'm wasted still
 I realize the time that I wasted here
 I feel like you can't feel the way I feel
 Oh, I'll be fucked up if you can't be right here
+
+[Pre-Chorus]
 Oh, ooh-woah
 Oh, ooh-woah
 Oh, ooh-woah
-I'll be fucked up if you can't be right here`
+Oh, I'll be fucked up if you can't be right here
+
+[Chorus]
+I do the same thing I told you that I never would
+I told you I'd change, even when I knew I never could
+I know that I can't find nobody else as good as you
+I need you to stay, need you to stay, hey
+
+[Verse 2]
+When I'm away from you, I miss your touch
+You're the reason I believe in love
+It's been difficult for me to trust
+And I'm afraid that I'ma fuck it up
+Ain't no way that I can leave you stranded
+'Cause you ain't never left me empty-handed
+And you know that I know that I can't live without you
+So, baby, stay
+
+[Pre-Chorus]
+Oh, ooh-woah
+Oh, ooh-woah
+Oh, ooh-woah
+I'll be fucked up if you can't be right here
+
+[Chorus]
+I do the same thing I told you that I never would
+I told you I'd change, even when I knew I never could
+I know that I can't find nobody else as good as you
+I need you to stay, need you to stay, hey
+
+[Outro]
+I need you to stay, need you to stay, hey`
   },
   {
     title: "As It Was",
-    lyrics: `[Verse 1]
+    lyrics: `[Intro]
+Come on, Harry, we wanna say goodnight to you
+
+[Verse 1]
 Holdin' me back
 Gravity's holdin' me back
 I want you to hold out the palm of your hand
@@ -223,6 +676,35 @@ You know it's not the same as it was
 In this world, it's just us
 You know it's not the same as it was
 As it was, as it was
-You know it's not the same`
+You know it's not the same
+
+[Verse 2]
+Answer the phone
+"Harry, you're no good alone
+Why are you sitting at home on the floor?
+What kind of pills are you on?"
+Ringin' the bell
+And nobody's coming to help
+Your daddy lives by himself
+He just wants to know that you're well, oh-oh-oh
+
+[Chorus]
+In this world, it's just us
+You know it's not the same as it was
+In this world, it's just us
+You know it's not the same as it was
+As it was, as it was
+You know it's not the same
+
+[Bridge]
+Go home, get ahead, light-speed internet
+I don't wanna talk about the way that it was
+Leave America, two kids follow her
+I don't wanna talk about who's doin' it first
+
+[Outro]
+As it was
+You know it's not the same as it was
+As it was, as it was`
   }
 ];
